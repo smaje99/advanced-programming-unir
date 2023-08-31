2b1f48a843b058b2727c840f2dbc06caa05b635d
Sergio Majé <smajefranco@gmail.com>
2023-08-30 20:53:33 -0500

    🎉(laboratory): Laboratory project initialize
    Signed-off-by: Sergio Majé <smajefranco@gmail.com>

---
+++ laboratory/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Laboratorio #1: Diseño e implementación de clasesTarea
+ *
+ * @author Sergio Andrés Majé Franco
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+    }
+}
